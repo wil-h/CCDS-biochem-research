1657c66796b7fdad328f6027ae872b66a7afb151
ccds-biochem <78439866+ccds-biochem@users.noreply.github.com>
2021-04-27 13:45:34 -0400

    Update torrision.r
    Actually spits out all required data, and it all ends up in the same excel file now.

--- torrision.r
+++ torrision.r
@@ -1,18 +1,38 @@
-setwd("~/") #moving back into the original directory
-#for debug purposes.
+setwd("~/")
 
-#install dependancies.
-#TODO, check if packages are already installed.
-install.packages("bio3d", dependencies=TRUE) 
-install.packages("devtools") 
-
-#import dependancies
+library(hash)
 library(devtools) 
 library(bio3d) 
-#install_bitbucket("Grantlab/bio3d", subdir = "ver_devel/bio3d/")  
 
-
-
+get_atom <- function (current_resno, starting_index, atom.data, elety)
+{
+  i = starting_index
+  if (substring(elety,nchar(elety)) == "-") {
+    i = i - 1
+    while(as.numeric(current_resno) - 1 == as.numeric(atom.data[i,3])) {
+      if (atom.data[i,4] == substring (elety,1,nchar (elety)-1)) {return (atom.data[i,5:7])}
+      i = i-1
+    }
+    return ("N/A")
+  }
+  else if (substring(elety,nchar(elety)) == "+")
+  {
+    while(current_resno == atom.data[i,3]) {i = i + 1}
+    while (as.numeric(current_resno) + 1 == as.numeric(atom.data[i,3]))
+    {
+      if (atom.data[i,4] == substring (elety,1,nchar (elety)-1)) {return (atom.data[i,5:7])}
+      i = i+1
+    }
+  }
+  else {
+    while(current_resno == atom.data[i,3]) 
+    {
+      if (atom.data[i,4] == elety && atom.data[i,3] == current_resno) {return (atom.data[i,5:7])}
+      i = i + 1
+    }
+  }
+  return ("N/A")
+}
 
 dir = getwd() 
 path = readline("enter relative path: ") #TODO support absolute and relative paths
@@ -20,30 +40,70 @@ path = readline("enter relative path: ") #TODO support absolute and relative pat
 print(paste(dir, "/", path, sep=''))
 setwd(paste(dir, "/", path, sep=''))
 
-
+colpaste <- function(x, col.names = colnames(x)) {
+  apply(x, 1, function(row) paste(row[col.names], collapse = ","))
+}
 
 #prompts the user to type in the console a three letter resid ID, such a PRO for proline
 resid <- readline(prompt="Enter 3 letter residue ID: ")
 
-pdbfiles <- list.files(pattern="*.pdb", full.names=TRUE) # Rerieving .pdb files 
+pdbfiles <- list.files(pattern="*.pdb", full.names=TRUE)
 
-#for loop that does the function for each pdb in pdbfiles vector
-for (i in pdbfiles){
-  #uses the read.pdb function in bio3d to store the pdb file from the working directory as a usable vector matrix with all the atoms and coordinates
-  pdb <- read.pdb(i)
+result_matrix = matrix(data = c("pdb code","chain","residue number", "CA-C-N-CA","C-N-CA-C","N-CA-C-N","N-CA-CB-CG","CA-CB-CG-CD1","CA-CB-CG-CD2","CA-C-N-CA"), nrow = 1, ncol = 10)
+
+row2 = c(" "," "," ","pre-omega","phi","psi","chi1","chi2a","chi2b","omega")
+result_matrix = rbind(result_matrix, row2)
+
+atoms = c ("CA","CB","CG","CD1","CD2","N","O","N+","C","C-","CA+","CA-")
+bonds = c ("CA-","C-", "N", "CA", "C-", "N","CA","C", "N", "CA", "C", "N+", "N", "CA", "CB", "CG", "CA", "CB", "CG", "CD1", "CA", "CB", "CG", "CD2", "CA", "C", "N+","CA+")
+
+
+
+for(current_file in pdbfiles){
   
-  #removes all data except date for the residue that was typed into console
+  pdb <- read.pdb(current_file)
   index <- trim(pdb, resid = resid, inds = NULL, sse = TRUE)
-  
-  #not quite sure what this does but there's errors otherwise... i think it removes null values
   index <- index[lapply(index,length)>0]
+  atom.data <- colpaste(pdb$atom, c("resid","chain","resno","elety","x", "y","z"))
+  atom.data <- matrix(unlist(strsplit(atom.data, "\\,")),ncol = 7, byrow = TRUE)
   
-  #prints the pdb file name
-  print(i)
+  #print(atom.data)
+  i = 1
   
-  #prints the table summary of the output of torsion.pdb function, the 'try' part means it only prints if the function runs without error
-  try(print(torsion.pdb(index)$tbl))
-  excel_data = torsion.pdb(index)$tbl
-  y <- as.character(excel_data) 
-  write.csv(excel_data, file = 'tor.csv', row.names = F) 
+  while(resid != atom.data[i,1]) {
+    i = i + 1
+    if(i == length(atom.data)/7) {
+      break #needed here in the unlikely event that a file has zero of the target
+      #residue present.
+    }
+  }
+  
+  while (i < length(atom.data)/7) {
+    current_resno = atom.data[i,3]
+    
+    coords <- hash()
+    for (atom in atoms) {
+      coords[[atom]] = get_atom (current_resno, i, atom.data, atom)
+    }
+    
+    angle_vector = c(current_file, atom.data[i,2],current_resno)
+    for (bond_index in 1:(length(bonds)/4))
+    {
+      array1 = get(bonds[bond_index * 4 - 3],coords)
+      array2= get(bonds[bond_index * 4 - 2], coords)
+      array3= get(bonds[bond_index * 4 - 1], coords)
+      array4= get(bonds[bond_index * 4], coords)
+      angle = torsion.xyz(c(as.numeric(array1),as.numeric(array2),as.numeric(array3),as.numeric(array4)))
+      angle_vector = c(angle_vector,angle)
+    }
+    result_matrix = rbind(result_matrix, angle_vector)
+    repeat { #since r does not have a "do while" loop, we need to use this instead
+      i = i + 1
+      if(i == length(atom.data)/7 || (current_resno != atom.data[i,3] && resid == atom.data[i,1])) {
+        break
+      }
+    }
+  }
 }
+
+write.matrix(result_matrix, file = "torsion.csv",sep=",") 
