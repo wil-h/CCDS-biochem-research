49c1ab4e21d356ef03321dbc799b7878230d8dfd
wil-h <123789183+wil-h@users.noreply.github.com>
2025-03-27 14:04:44 -0400

    2025 work

--- collect_data.py
+++ collect_data.py
@@ -28,6 +28,18 @@ cw_dir = os.getcwd().replace("\\", "/")
 out_dir = cw_dir + "/" + config_data["output_dir"]
 
 os.chdir(out_dir)
+try:
+    os.remove("bond_length" + config_data['resid'] + ".csv")
+except:
+    None
+try:
+    os.remove("bond_angle" + config_data['resid'] + ".csv")
+except:
+    None
+try:
+    os.remove("torsion" + config_data['resid'] + ".csv")
+except:
+    None
 
 files = os.listdir()
 pdbs = []
@@ -47,7 +59,6 @@ for array_num in range(cores):
     task_file.close()
 
 processes = [Popen('"' + config_data["rscript_path"] + '" --vanilla worker.r '+ str(i + 1) + ' ' + sys.argv[1] + ' ' + config_data["libpaths"][0]+ ' ' + config_data["libpaths"][1], shell=False) for i in range(cores)]
-
 exitcodes = [p.wait() for p in processes]   
 
 os.chdir(out_dir)
@@ -55,6 +66,18 @@ files = os.listdir()
 csv_delimeters = ["a","l","t"]
 csvs = [[],[],[]]
 matrices = [[],[],[]]
+
+#clean weird first row in some csvs:
+for filepath in files:
+    if filepath[-4:] == ".csv" and "torsion" in filepath:
+        with open(filepath, mode='r', newline="") as file:
+            reader=list(csv.reader(file))
+            firstval=reader[0][0]
+        if "pdb code" not in firstval:
+            modified = reader[1:] 
+            with open(filepath, mode='w', newline="") as file:
+                writer = csv.writer(file)
+                writer.writerows(modified)
 
 for delimeter in csv_delimeters:
     for filename in files:
@@ -81,6 +104,94 @@ with open("torsion" + config_data['resid'] + ".csv","w",newline='') as my_csv:
 for category in csvs:
     for filename in category:
         os.remove(filename)
+
+#clean issues in the torsion output:
+def valid_resnos(pdbname):
+    with open("bond_length" + config_data['resid'] + ".csv","r") as lengthCsv:
+        reader = csv.reader(lengthCsv)
+        lengthmatrix = [row for row in reader]
+    validresnos=[]    
+    i=0
+    while(lengthmatrix[i][0][:7]!=pdbname[:7]):
+        i+=1
+    for row in range(i,len(lengthmatrix)):
+        chain=lengthmatrix[row][1][0]
+        resno=int(lengthmatrix[row][2])
+        if (chain, resno) not in validresnos:
+            validresnos.append((chain, resno))
+        else:
+            break
+    return validresnos
+
+def closest_integer(item, resnos):
+    closest = 10000000
+    chain=item[0]
+    for k in resnos:
+        if (abs(int(item[1]) - closest) > abs(int(item[1]) - k[1])) and chain==k[0]:
+            closest = k[1]
+    return closest
+
+def lowestRowOccupancyValue(row):
+    lowest=1
+    for i in range(4,16,2):
+        occ=float(row[i])
+        if occ<lowest:
+            lowest=float(row[i])
+    return lowest
+
+def align_matrix(matrix):
+    col_widths = [max(len(str(row[i])) for row in matrix) for i in range(len(matrix[0]))]
+    aligned_matrix = [
+        [str(row[i]).ljust(col_widths[i]) for i in range(len(row))]
+        for row in matrix
+    ]
+    return aligned_matrix
+
+with open("torsion" + config_data['resid'] + ".csv","r") as torsionCsv:
+    reader = csv.reader(torsionCsv)
+    torsionmatrix = [row for row in reader]
+subtractor=0
+prevpdb=""
+for row in range(3,len(torsionmatrix)):
+    resno=(torsionmatrix[row][1][0], int(torsionmatrix[row][2]))
+    pdbname=torsionmatrix[row][0]
+    validresnos=valid_resnos(pdbname)
+
+    if resno not in validresnos:
+        closestResno=closest_integer(resno, validresnos)
+        torsionmatrix[row][2]=closestResno
+
+
+prevresno=0
+for index in range(3,len(torsionmatrix)):
+    row=torsionmatrix[index]
+    try:
+        if int(row[2])==int(prevresno):
+            torsionmatrix[index-1][2]=str(row[2])+"A"
+            torsionmatrix[index][2]=str(row[2])+"B"
+
+            #make sure the occupancy values line up:
+            occ1=lowestRowOccupancyValue(torsionmatrix[index-1])
+            occ2=lowestRowOccupancyValue(torsionmatrix[index])
+            if occ1+occ2!=1:
+                occ1=1-occ2
+            
+            #apply the occupancy to the whole row
+            for i in range(4,18,2):
+                torsionmatrix[index-1][i]=round(float(occ1),2)
+            for i in range(4,18,2):
+                torsionmatrix[index][i]=round(float(occ2),2)
+    except:
+        None
+
+    prevresno=torsionmatrix[index][2]
+
+#write the corrected torsion matrix
+torsionmatrix=align_matrix(torsionmatrix)
+os.remove("torsion" + config_data['resid'] + ".csv")
+with open("torsion" + config_data['resid'] + ".csv","w",newline='') as my_csv:
+    newarray = csv.writer(my_csv,delimiter=',')
+    newarray.writerows(torsionmatrix)
 
 os.chdir(cw_dir)
 for i in range(cores):
--- oldworker.r
+++ oldworker.r
@@ -1,3 +1,6 @@
+#this is very similar to the other worker, but it does not account for splitting with occupancy values.
+#if there are two different conformations from a single pdb, it will just choose one to put in the spreadsheet. 
+#if your AA does not have any splitting, then it is okay to use this version.
 options(warn=-1)
 args = commandArgs(trailingOnly=TRUE)
 .libPaths(c(args[3],args[4]))
@@ -66,48 +69,6 @@ calc_bondlength <- function (array1, array2)
   return (length) 
 }
 
-closest_integer <- function(item, chain, resnos){
-  closest=10000000
-  k=1
-  while(k<=length(resnos)){
-    if(abs(as.integer(item)-closest)>abs(as.integer(item)-as.integer(resnos[k])) && chain==resnos[k+1]){
-      closest=as.integer(resnos[k])
-    }
-    k=k+2
-  }
-  return(closest)
-}
-
-#find the valid resnos to use the filler data
-valid_pdb_resnos <- function(atomdata,resid){
-  i=1
-  while(i<=nrow(atomdata)){
-    if(atomdata[i,1]!=resid){
-      i=i+1
-    }
-    else{
-      break
-    }
-  }
-  valid_resnos <- c()
-  while(i<=nrow(atomdata)){
-    value <- atomdata[i,3]
-    chain <- atomdata[i,2]
-    if(atomdata[i,1]==resid){
-      prevvalue=valid_resnos[length(valid_resnos)-1]
-      if(is.null(prevvalue)){
-        prevvalue=0
-      }
-      if (value != "torsion vector" && prevvalue != value) {
-        valid_resnos <- c(valid_resnos, value)  
-        valid_resnos <- c(valid_resnos, chain) 
-      }
-    }
-    i=i+1
-  }
-  return(valid_resnos)
-}
-
 ############################
 #boring initializaton stuff#
 ############################
@@ -125,10 +86,8 @@ angle_matrix = rbind(angle_matrix, angle_row2)
 
 torsion_header = config$torsion$header
 torsion_matrix = matrix(data = torsion_header, nrow = 1, ncol = length(torsion_header))
-filler_torsion_matrix = matrix(data = torsion_header, nrow = 1, ncol = length(torsion_header))
 torsion_row2 = config$torsion$header2
 torsion_matrix = rbind(torsion_matrix, torsion_row2)
-filler_torsion_matrix = rbind(filler_torsion_matrix, torsion_row2)
 torsion_bonds = config$torsion$bonds
 
 length_header = config$length$header
@@ -154,65 +113,11 @@ for(file_index in 1:length(pdbfiles)){
   index <- index[lapply(index,length)>0]
   atom.data <- colpaste(pdb$atom, c("resid","chain","resno","elety","x", "y","z","o","alt"))
   atom.data <- matrix(unlist(strsplit(atom.data, "\\,")),ncol = 9, byrow = TRUE)
-  atom.data.torsion <- matrix(ncol=9,byrow=TRUE)
-
-  #ensure there are no preceding spaces in the atom data resnos
-  for(i in 1:(length(atom.data)/9)){
-    atom.data[i,3]=gsub(" ","",atom.data[i,3])
-  }
-
-  #find split branches and reformat them for torsion
-  subnum=0
-  atom_index=1
-  while(atom_index<=(length(atom.data)/9)){
-    if(atom.data[atom_index,1]==resid && atom.data[atom_index,9]!="NA"){
-      affectedResno=atom.data[atom_index,3]
-      affectedStrand=atom.data[atom_index,2]
-      resAtoms=matrix(ncol=9,byrow=TRUE)
-      for(l in 1:(length(atom.data)/9)){
-        if(atom.data[l,3]==affectedResno && atom.data[l,2]==affectedStrand){
-          resAtoms=rbind(resAtoms,atom.data[l,])
-        }
-      }
-      resAtoms=resAtoms[-1,]
-      aRes=matrix(ncol=9,byrow=TRUE)
-      bRes=matrix(ncol=9,byrow=TRUE)
-      for(l in 1:(length(resAtoms)/9)){
-        if(resAtoms[l,9]=="NA"){
-          aRes=rbind(aRes,resAtoms[l,])
-          bRes=rbind(bRes,resAtoms[l,])
-        }
-        if(resAtoms[l,9]=="A"){
-          aRes=rbind(aRes,resAtoms[l,])
-        }
-        if(resAtoms[l,9]=="B"){
-          bRes=rbind(bRes,resAtoms[l,])
-        }
-      }
-      aRes=aRes[-1,]
-      bRes=bRes[-1,]
-      for (i in 1:nrow(bRes)){
-        bRes[i, 3]=as.character(as.integer(bRes[i, 3]) + 1) 
-      }
-
-      atom.data.torsion = rbind(atom.data.torsion, unname(aRes))
-      atom.data.torsion = rbind(atom.data.torsion, unname(bRes))
-      subnum=subnum+1
-      while(atom.data[atom_index,3]==affectedResno){
-        atom_index=atom_index+1
-      }
-
-    }
-    else{
-      row=atom.data[atom_index,]
-      row[3]=as.character(as.integer(row[3])+subnum)
-      atom.data.torsion = rbind(atom.data.torsion, unname(row))
-    }
-    atom_index=atom_index+1
-  }
-  atom.data.torsion=atom.data.torsion[-1,]#remove the blank first row
+  
+  print(atom.data)
 
   i = 1
+  
   while(resid != atom.data[i,1]) {
     i = i + 1
     if(i == length(atom.data)/9) {
@@ -267,9 +172,13 @@ for(file_index in 1:length(pdbfiles)){
         },
         error=function (e){torsional_angle="NA"})
       torsion_vector = c(torsion_vector,torsional_angle)#adds an angle each time
-      torsion_vector = c(torsion_vector,"OCC")#adds an occupancy placeholder
+      bondOccupancy= atom.data[i,8]
+      torsion_vector = c(torsion_vector,bondOccupancy)
+      #print(c("angle:",torsional_angle))
+      #print(c("occupancy:",bondOccupancy))
+      #print(torsion_vector)
     }
-    filler_torsion_matrix = rbind(filler_torsion_matrix, torsion_vector)
+    torsion_matrix = rbind(torsion_matrix, torsion_vector)
     
     repeat { #since r does not have a "do while" loop, we need to use this instead
       i = i + 1
@@ -278,81 +187,9 @@ for(file_index in 1:length(pdbfiles)){
       }
     }
   }
-  #repeat for torsion with different atom data
-    i = 1
-  while(resid != atom.data[i,1]) {
-    i = i + 1
-    if(i == length(atom.data)/9) {
-      break #needed here in the unlikely event that a file has zero of the target
-      #residue present.
-    }
-  }
-
-  while (i < length(atom.data.torsion)/9) {
-    current_resno = atom.data.torsion[i,3]
-    coords <- hash()
-    for (atom in atoms) {
-      coords[[atom]] = get_atom (current_resno, i, atom.data.torsion, atom)
-    }
-    torsion_vector = c(current_file, atom.data.torsion[i,2],current_resno)
-
-    for (bond_index in 1:(length(torsion_bonds)/4))
-    {
-      tryCatch(
-        expr = {
-          array1 = get(torsion_bonds[bond_index * 4 - 3],coords)
-          array2= get(torsion_bonds[bond_index * 4 - 2], coords)
-          array3= get(torsion_bonds[bond_index * 4 - 1], coords)
-          array4= get(torsion_bonds[bond_index * 4], coords)
-          torsional_angle = torsion.xyz(c(as.numeric(array1),as.numeric(array2),as.numeric(array3),as.numeric(array4)))
-
-        },
-        error=function (e){torsional_angle="NA"})
-      torsion_vector = c(torsion_vector,torsional_angle)#adds an angle each time
-      bondOccupancy= atom.data.torsion[i+bond_index,8]
-      torsion_vector = c(torsion_vector,bondOccupancy)
-    }
-    torsion_matrix = rbind(torsion_matrix, torsion_vector)
-
-    #now go through with the filler data and fill in any blanks that were caused by the modified atom data
-    p=1
-    while(p<nrow(torsion_matrix)){
-      if(torsion_matrix[p,1]==current_file){
-        break
-      }
-      else{
-        p=p+1
-      }
-    }
-    valid_resnos=valid_pdb_resnos(atom.data,resid)
-    for(row in p:nrow(torsion_matrix)){
-      for(column in 4:ncol(torsion_matrix)){
-        if(torsion_matrix[row,column]==0){
-          closest = closest_integer(torsion_matrix[row,3],torsion_matrix[row,2],valid_resnos)
-          #closest working, now just need to change the filling logic below
-          k=1
-          while(k<=nrow(filler_torsion_matrix)){
-            if(filler_torsion_matrix[k,1]==current_file && as.integer(filler_torsion_matrix[k,3])==as.integer(closest)){
-              break
-            }
-            else{
-              k=k+1
-            }
-          }
-          torsion_matrix[row,column]=filler_torsion_matrix[k,column]
-        }
-      }
-    } 
-    
-    repeat { #since r does not have a "do while" loop, we need to use this instead
-      i = i + 1
-      if(i == length(atom.data.torsion)/9 || (current_resno != atom.data.torsion[i,3] && resid == atom.data.torsion[i,1])) {
-        break
-      }
-    }
-  }
 }
+
 
 write.matrix(angle_matrix, file = paste("angle_",args[1],".csv",sep=""),sep=",") 
 write.matrix(length_matrix, file = paste("length_",args[1],".csv",sep=""),sep=",") 
-write.matrix(torsion_matrix, file = paste("torsion_",args[1],".csv",sep=""),sep=",") #there are some issues with the torsion output, but they are cleaned up in collect_data.py
+write.matrix(torsion_matrix, file = paste("torsion_",args[1],".csv",sep=""),sep=",") 
